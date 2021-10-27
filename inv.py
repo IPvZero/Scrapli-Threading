be0d98a2507ee44bd4e45b633a4d9cffac97f49d
IPvZero <46572101+IPvZero@users.noreply.github.com>
2021-10-27 15:19:00 +0100

    Create inv.py

---
+++ inv.py
@@ -0,0 +1,8 @@
+DEVICES = [
+    {"hostname": "R1", "host": "192.168.31.101"},
+    {"hostname": "R2", "host": "192.168.31.102"},
+    {"hostname": "R3", "host": "192.168.31.103"},
+    {"hostname": "R4", "host": "192.168.31.104"},
+    {"hostname": "R5", "host": "192.168.31.105"},
+    {"hostname": "R6", "host": "192.168.31.106"},
+]
